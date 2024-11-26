480e86e8729e377f57d9af57e1fb821265b1f822
Daniel Siu <daniel.siu@oocl.com>
2024-11-26 17:23:23 +0800

    feat: migrate employee service

--- src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
@@ -48,11 +48,11 @@ public class EmployeeService {
     }
 
     public Employee update(Integer employeeId , Employee employee) {
-        Employee employeeExisted = employeeInMemoryRepository.findById(employeeId);
+       Employee employeeExisted = employeeRepository.findById(employeeId).orElse(null);
         if(!employeeExisted.getActive())
             throw new EmployeeInactiveException();
 
-        return employeeInMemoryRepository.update(employeeId, employee);
+        return employeeRepository.save(employee);
     }
 
     public void delete(Integer employeeId) {
