b9337c0e84b03e3933a72de4cf967b0ac206996a
Daniel Siu <daniel.siu@oocl.com>
2024-11-26 21:41:34 +0800

    feat: implement return paged companies by page params

--- src/test/java/com/oocl/springbootemployee/controller/CompanyControllerTest.java
+++ src/test/java/com/oocl/springbootemployee/controller/CompanyControllerTest.java
@@ -92,21 +92,21 @@ class CompanyControllerTest {
                 .usingRecursiveFieldByFieldElementComparator()
                 .isEqualTo(givenCompanies);
     }
-//
-//    @Test
-//    void should_return_paged_companies_when_get_by_page_params() throws Exception {
-//        // Given
-//        var pageIndex = 2;
-//        var pageSize = 2;
-//        final var the5thEmployeeCompanyInPage3 = companyRepository.findById(nexus_industries.getId());
-//        // When
-//        // Then
-//        client.perform(MockMvcRequestBuilders.get(String.format("/companies?pageIndex=%s&pageSize=%s", pageIndex, pageSize)))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(the5thEmployeeCompanyInPage3.get().getId()))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(the5thEmployeeCompanyInPage3.get().getName()));
-//    }
+
+    @Test
+    void should_return_paged_companies_when_get_by_page_params() throws Exception {
+        // Given
+        var pageIndex = 2;
+        var pageSize = 2;
+        final var the5thEmployeeCompanyInPage3 = companyRepository.findById(nexus_industries.getId());
+        // When
+        // Then
+        client.perform(MockMvcRequestBuilders.get(String.format("/companies?pageIndex=%s&pageSize=%s", pageIndex, pageSize)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(the5thEmployeeCompanyInPage3.get().getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(the5thEmployeeCompanyInPage3.get().getName()));
+    }
 //
 //    @Test
 //    void should_return_employees_when_get_employees_under_the_company() throws Exception {
